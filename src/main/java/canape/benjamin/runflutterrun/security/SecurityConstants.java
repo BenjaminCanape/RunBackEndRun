fd9a0724ef0913b15cd7be2f2fbca39c6d80414e
Benjamin Canape <benjamin.canape@gmail.com>
2023-06-25 16:01:33 +0200

    expire token in 10h to be able to save most of activities. Later, a refresh token will be added

--- src/main/java/canape/benjamin/runflutterrun/security/SecurityConstants.java
+++ src/main/java/canape/benjamin/runflutterrun/security/SecurityConstants.java
@@ -3,7 +3,7 @@ package canape.benjamin.runflutterrun.security;
 public class SecurityConstants {
 
     public static final String SECRET = "SECRET_KEY";
-    public static final long EXPIRATION_TIME = 900_000;
+    public static final long EXPIRATION_TIME = 36_000_000;
     public static final String SIGN_UP_URL = "/api/user/login";
     public static final String REGISTER_URL = "/api/user/register";
 
